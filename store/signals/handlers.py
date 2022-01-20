5820d9e303af187b040004e8f124c48ce071f3bb
Mahdi Ghiasi <mahdiw.ghiasi@gmail.com>
2022-01-20 16:54:48 +0330

    Formatted and Improved Code Style

--- store/signals/handlers.py
+++ store/signals/handlers.py
@@ -4,6 +4,6 @@ from django.dispatch import receiver
 from store.models import Customer
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
-def create_custome_for_new_user(sender, **kwargs):
+def create_customer_for_new_user(sender, **kwargs):
     if kwargs['created']:
         Customer.objects.create(user=kwargs['instance'])
--- store/tests/conftest.py
+++ store/tests/conftest.py
@@ -2,12 +2,15 @@ from django.contrib.auth.models import User
 from rest_framework.test import APIClient
 import pytest
 
+
 @pytest.fixture
 def api_client():
-   return APIClient()
+    return APIClient()
+
 
 @pytest.fixture
 def authenticate(api_client):
-   def do_authenticate(is_staff=False):
-      api_client.force_authenticate(user=User(is_staff=is_staff))
-   return do_authenticate
+    def do_authenticate(is_staff=False):
+        api_client.force_authenticate(user=User(is_staff=is_staff))
+
+    return do_authenticate
