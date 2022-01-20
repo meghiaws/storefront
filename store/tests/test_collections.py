e0d49738f5d5c9c5fe5bbd88691ae40cf054c69e
Mahdi Ghiasi <mahdiw.ghiasi@gmail.com>
2022-01-20 09:10:18 +0330

    Added Retrive and Create Tests for Collection

---
+++ store/tests/test_collections.py
@@ -0,0 +1,62 @@
+from store.models import Collection
+from rest_framework import status
+import pytest
+from model_bakery import baker
+
+@pytest.fixture
+def create_collection(api_client):
+    def do_create_collection(collection):
+        return api_client.post('/store/collection/', collection)
+    return do_create_collection
+
+
+@pytest.mark.django_db
+class TestCreateCollection:
+    def test_if_user_is_anonymous_returns_401(self, create_collection):
+        response = create_collection({'title': 'a'})
+
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+    def test_if_user_is_not_admin_returns_403(self, authenticate, create_collection):
+        authenticate()
+
+        response = create_collection({'title': 'a'})
+
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+
+    def test_if_data_is_invalid_returns_400(self, authenticate, create_collection):
+        authenticate(is_staff=True)
+
+        response = create_collection({'title': ''})
+
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert response.data['title'] is not None
+
+    def test_if_data_is_valid_returns_201(self, authenticate, create_collection):
+        authenticate(is_staff=True)
+
+        response = create_collection({'title': 'a'})
+
+        assert response.status_code == status.HTTP_201_CREATED
+        assert response.data['id'] > 0
+
+
+@pytest.mark.django_db
+class TestRetriveCollection:
+    def test_if_collection_doesnt_exist_return_404(self, api_client):
+
+        response = api_client.get('/store/collection/8/')
+
+        assert response.status_code == status.HTTP_404_NOT_FOUND
+
+    def test_if_collection_exists_return_200(self, api_client):
+        collection = baker.make(Collection)
+
+        response = api_client.get(f'/store/collection/{collection.id}/')
+
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data == {
+            'id': collection.id,
+            'title': collection.title,
+            'products_count': 0
+        }
