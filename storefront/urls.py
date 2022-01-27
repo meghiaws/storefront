bd4cb6a27e527b60321f1cd0fa5c03ee0b72d762
Mahdi Ghiasi <mahdiw.ghiasi@gmail.com>
2022-01-27 21:24:44 +0330

    Homepage Added

--- storefront/urls.py
+++ storefront/urls.py
@@ -8,6 +8,7 @@ admin.site.site_header = 'Storefront Admin'
 admin.site.index_title = 'Admin'
 
 urlpatterns = [
+    path('', include('core.urls')),
     path('admin/', admin.site.urls),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
