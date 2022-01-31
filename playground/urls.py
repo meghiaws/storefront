7a61dfff04ec6c49d41653e132c605d62bcda29d
Mahdi Ghiasi <mahdiw.ghiasi@gmail.com>
2022-01-31 12:15:56 +0330

    Added Gunicorn

--- playground/urls.py
+++ playground/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 # URLConf
 urlpatterns = [
-    path('hello/', views.say_hello)
+    path('hello/', views.HelloView.as_view())
 ]
--- playground/views.py
+++ playground/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
+from django.core.cache import cache
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
+from rest_framework.views import APIView
+import requests
+import logging
 
+logger = logging.getLogger(__name__)
 
-def say_hello(request):
-    return render(request, 'hello.html', {'name': 'Mosh'})
+class HelloView(APIView):
+    def get(self, request):
+        try:
+            logger.info('Calling httpbin')
+            response = requests.get('https://httpbin.org/delay/2')
+            logger.info('Response received')
+            data = response.json()
+        except requests.ConnectionError:
+            logger.critical('httpbin is offline')
+        return render(request, 'hello.html', {'name': data})
