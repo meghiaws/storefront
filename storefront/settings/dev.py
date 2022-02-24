0fe7363c879bfcfd81ced8002dc224d755532c70
Mahdi Ghiasi <mahdiw.ghiasi@gmail.com>
2022-02-24 04:25:36 +0330

    Completed dev settings

--- storefront/settings/dev.py
+++ storefront/settings/dev.py
@@ -1,16 +1,28 @@
+import environ      
 from .common import *
+
+env = environ.Env()
+environ.Env.read_env(BASE_DIR / '.env')
 
 DEBUG = True
 
 SECRET_KEY = 'django-insecure-hs6j037urx6iav+7#10%-vu4l4f5@@-1_zo)oft4g7$vf2$jmp'
 
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'storefront3',
-        'USER': 'postgres',
-        'PASSWORD': '1342917',
-        'HOST': 'localhost',
-        'PORT': '5432',
+    'default': env.db()
+}
+
+CELERY_BROKER_URL = 'redis://redis:6379/1'
+
+CACHES = { 
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://redis:6379/2",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
     }
 }
+INSTALLED_APPS += ['silk']
