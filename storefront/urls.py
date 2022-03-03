dd4a96b204a3ebdf8137739ad1f94d3d6f1579b4
Mahdi Ghiasi <mahdiw.ghiasi@gmail.com>
2022-03-03 05:13:32 +0330

    New endpoints for OpenAPI Schemas

--- storefront/urls.py
+++ storefront/urls.py
@@ -1,29 +1,31 @@
-from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
 import debug_toolbar
 
-admin.site.site_header = 'Storefront Admin'
-admin.site.index_title = 'Admin'
+admin.site.site_header = "Storefront Admin"
+admin.site.index_title = "Admin"
 
 
 urlpatterns = [
-    path('', include('core.urls')),
-    path('admin/', admin.site.urls),
-    path('auth/', include('djoser.urls')),
-    path('auth/', include('djoser.urls.jwt')),
-    path('playground/', include('playground.urls')),
-    path('store/', include('store.urls')),
-
-    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
-    path('__debug__/', include(debug_toolbar.urls)),
+    path("", include("core.urls")),
+    path("admin/", admin.site.urls),
+    path("auth/", include("djoser.urls")),
+    path("auth/", include("djoser.urls.jwt")),
+    path("playground/", include("playground.urls")),
+    path("store/", include("store.urls")),
+    path("schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
+    path("redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
+    path("__debug__/", include(debug_toolbar.urls)),
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
-    urlpatterns += [path('silk/', include('silk.urls', namespace='silk'))]
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += [path("silk/", include("silk.urls", namespace="silk"))]
