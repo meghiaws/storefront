1746f3b1cbefb32e6da888c1bc1dd941b72dbc71
Mahdi Ghiasi <mahdiw.ghiasi@gmail.com>
2022-03-06 03:58:22 +0330

    Add ALLOWED_HOST to prod settings

--- storefront/settings/prod.py
+++ storefront/settings/prod.py
@@ -2,12 +2,15 @@ import os
 import environ
 from .common import *
 
+
 env = environ.Env()
 environ.Env.read_env(BASE_DIR / '.env')
 
 DEBUG = False
 
 SECRET_KEY = os.environ['SECRET_KEY']
+
+ALLOWED_HOSTS = env.list('ALLOWED_HOST')
 
 DATABASES = {
     'default': env.db()
