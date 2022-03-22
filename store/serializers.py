19c60c4d8faae83a05ab3447235efea336ecbb5c
Mahdi Ghiasi <mahdiw.ghiasi@gmail.com>
2022-03-22 05:22:46 +0430

    Formating the serializers.py

--- store/serializers.py
+++ store/serializers.py
@@ -6,14 +6,23 @@ from django.db.models import fields
 from rest_framework import serializers
 
 from .signals import order_created
-from .models import (Cart, CartItem, Collection, Customer, Order, OrderItem,
-                     Product, ProductImage, Review)
+from .models import (
+    Cart,
+    CartItem,
+    Collection,
+    Customer,
+    Order,
+    OrderItem,
+    Product,
+    ProductImage,
+    Review,
+)
 
 
 class CollectionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Collection
-        fields = ['id', 'title', 'products_count']
+        fields = ["id", "title", "products_count"]
 
     products_count = serializers.IntegerField(read_only=True)
 
@@ -21,24 +30,35 @@ class CollectionSerializer(serializers.ModelSerializer):
 class ProductImageSerializer(serializers.ModelSerializer):
     class Meta:
         model = ProductImage
-        fields = ['id', 'image']
+        fields = ["id", "image"]
 
     def save(self, **kwargs):
         return super().save(**kwargs)
 
     def create(self, validated_data):
-        return ProductImage.objects.create(product_id=self.context['product_id'], **validated_data)
+        return ProductImage.objects.create(
+            product_id=self.context["product_id"], **validated_data
+        )
 
 
 class ProductListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ['id', 'title', 'slug', 'description',
-                  'price', 'tax', 'collection', 'images']
+        fields = [
+            "id",
+            "title",
+            "slug",
+            "description",
+            "price",
+            "tax",
+            "collection",
+            "images",
+        ]
 
     images = ProductImageSerializer(many=True, read_only=True)
     price = serializers.DecimalField(
-        max_digits=6, decimal_places=2, source='unit_price')
+        max_digits=6, decimal_places=2, source="unit_price"
+    )
     tax = serializers.SerializerMethodField(method_name="calculate_tax")
     collection = serializers.StringRelatedField(read_only=True)
 
@@ -49,19 +69,35 @@ class ProductListSerializer(serializers.ModelSerializer):
 class ProductCreateUpdateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ['id', 'title', 'slug', 'description',
-                  'unit_price', 'inventory', 'collection']
+        fields = [
+            "id",
+            "title",
+            "slug",
+            "description",
+            "unit_price",
+            "inventory",
+            "collection",
+        ]
 
 
 class ProductDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ['id', 'title', 'slug', 'description',
-                  'price', 'tax', 'collection', 'images']
+        fields = [
+            "id",
+            "title",
+            "slug",
+            "description",
+            "price",
+            "tax",
+            "collection",
+            "images",
+        ]
 
     images = ProductImageSerializer(many=True, read_only=True)
     price = serializers.DecimalField(
-        max_digits=6, decimal_places=2, source='unit_price')
+        max_digits=6, decimal_places=2, source="unit_price"
+    )
     tax = serializers.SerializerMethodField(method_name="calculate_tax")
     collection = CollectionSerializer(read_only=True)
 
@@ -69,27 +105,26 @@ class ProductDetailSerializer(serializers.ModelSerializer):
         return product.unit_price * Decimal(1.1)
 
 
-
 class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review
-        fields = ['id', 'date', 'name', 'description']
+        fields = ["id", "date", "name", "description"]
 
     def create(self, validated_data):
-        product_id = self.context['product_id']
+        product_id = self.context["product_id"]
         return Review.objects.create(product_id=product_id, **validated_data)
 
 
 class SimpleProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ['id', 'title', 'unit_price']
+        fields = ["id", "title", "unit_price"]
 
 
 class CartItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = CartItem
-        fields = ['id', 'product', 'quantity', 'total_price']
+        fields = ["id", "product", "quantity", "total_price"]
 
     product = SimpleProductSerializer()
     total_price = serializers.SerializerMethodField()
@@ -101,21 +136,22 @@ class CartItemSerializer(serializers.ModelSerializer):
 class CartSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cart
-        fields = ['id', 'items', 'total_price']
+        fields = ["id", "items", "total_price"]
 
     id = serializers.UUIDField(read_only=True)
     items = CartItemSerializer(many=True, read_only=True)
     total_price = serializers.SerializerMethodField()
 
     def get_total_price(self, cart: Cart):
-        return sum([item.quantity * item.product.unit_price for item in cart.items.all()])
+        return sum(
+            [item.quantity * item.product.unit_price for item in cart.items.all()]
+        )
 
 
 class AddCartItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = CartItem
-
-        fields = ['id', 'product_id', 'quantity']
+        fields = ["id", "product_id", "quantity"]
 
     def validate_product_id(self, value):
         try:
@@ -123,23 +159,22 @@ class AddCartItemSerializer(serializers.ModelSerializer):
         except Product.DoesNotExist:
             product = None
         if product is None:
-            raise serializers.ValidationError(
-                'No product with given ID was found.')
+            raise serializers.ValidationError("No product with given ID was found.")
         return value
 
     def save(self, **kwargs):
-        cart_id = self.context['cart_id']
-        product_id = self.validated_data['product_id']
-        quantity = self.validated_data['quantity']
+        cart_id = self.context["cart_id"]
+        product_id = self.validated_data["product_id"]
+        quantity = self.validated_data["quantity"]
         try:
-            cart_item = CartItem.objects.get(
-                cart_id=cart_id, product_id=product_id)
+            cart_item = CartItem.objects.get(cart_id=cart_id, product_id=product_id)
             cart_item.quantity += quantity
-            cart_item.save(update_fields=['quantity'])
+            cart_item.save(update_fields=["quantity"])
             self.instance = cart_item
         except CartItem.DoesNotExist:
             self.instance = CartItem.objects.create(
-                cart_id=cart_id, **self.validated_data)
+                cart_id=cart_id, **self.validated_data
+            )
         return self.instance
 
     product_id = serializers.IntegerField()
@@ -148,13 +183,13 @@ class AddCartItemSerializer(serializers.ModelSerializer):
 class UpdateCartItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = CartItem
-        fields = ['quantity']
+        fields = ["quantity"]
 
 
 class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customer
-        fields = ['id', 'user_id', 'phone', 'birth_date', 'membership']
+        fields = ["id", "user_id", "phone", "birth_date", "membership"]
 
     user_id = serializers.IntegerField()
 
@@ -162,7 +197,7 @@ class CustomerSerializer(serializers.ModelSerializer):
 class OrderItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = OrderItem
-        fields = ['id', 'product', 'unit_price', 'quantity', 'product']
+        fields = ["id", "product", "unit_price", "quantity", "product"]
 
     product = SimpleProductSerializer()
 
@@ -170,45 +205,46 @@ class OrderItemSerializer(serializers.ModelSerializer):
 class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
-        fields = ['id', 'customer', 'placed_at', 'payment_status', 'items']
+        fields = ["id", "customer", "placed_at", "payment_status", "items"]
 
     items = OrderItemSerializer(many=True)
+
 
 class UpdateOrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
-        fields = ['payment_status']
+        fields = ["payment_status"]
+
 
 class CreateOrderSerializer(serializers.Serializer):
     cart_id = serializers.UUIDField()
 
     def validate_cart_id(self, cart_id):
         if not Cart.objects.filter(pk=cart_id).exists():
-            raise serializers.ValidationError(
-                'No Cart was Found with Given ID.'
-            )
+            raise serializers.ValidationError("No Cart was Found with Given ID.")
         if CartItem.objects.filter(cart_id=cart_id).count() == 0:
-            raise serializers.ValidationError('The cart is empty.')
+            raise serializers.ValidationError("The cart is empty.")
         return cart_id
 
     def save(self, **kwargs):
         with transaction.atomic():
-            cart_id = self.validated_data['cart_id']
+            cart_id = self.validated_data["cart_id"]
 
-            customer = Customer.objects.get(user_id=self.context['user_id'])
+            customer = Customer.objects.get(user_id=self.context["user_id"])
 
             order = Order.objects.create(customer=customer)
-            cart_items = CartItem.objects \
-                .select_related('product') \
-                .filter(cart_id=cart_id)
+            cart_items = CartItem.objects.select_related("product").filter(
+                cart_id=cart_id
+            )
 
             order_items = [
                 OrderItem(
                     order=order,
                     product=item.product,
                     unit_price=item.product.unit_price,
-                    quantity=item.quantity
-                ) for item in cart_items.all()
+                    quantity=item.quantity,
+                )
+                for item in cart_items.all()
             ]
             OrderItem.objects.bulk_create(order_items)
             Cart.objects.filter(pk=cart_id).delete()
